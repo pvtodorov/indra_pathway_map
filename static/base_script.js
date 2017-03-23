e27cf5ab36607990d6da6e7668a51f6ea67b2823
Petar Todorov <petar.v.todorov@gmail.com>
2017-03-23 14:32:09 -0400

    Add CiteAb to tooltips

--- static/base_script.js
+++ static/base_script.js
@@ -415,6 +415,7 @@ function drawCytoscape (div_id, model_response) {
 
           var list_lines = content.map(function( link ){
           var line = '<b style="font-size:13px">' + String(link[0].id) + '</b>' + ' ' +
+                     '<a  style="font-size:11px" target="_blank" href=https://www.citeab.com/search?q="' + link[0].id + '">' +  "CiteAb"  + '</a>&nbsp;' +
                      '<a  style="font-size:11px" target="_blank" href="' + link[0].url + '">' + link[0].name + '</a>&nbsp;' +
                      '<a style="font-size:11px" target="_blank" href="' + link[1].url + '">' + link[1].name  + '</a>';
           return line;
@@ -462,11 +463,14 @@ function drawCytoscape (div_id, model_response) {
         }
         else {
           var content_text = [];
+          content_text.push(
+              {name : "CiteAb", url: "https://www.citeab.com/search?q=" + n.data().name});
           if (data.hasOwnProperty("db_refs")){
             db_refs = data.db_refs;
             for (var namespace in db_refs) {
               content_text.push(
                 {name : namespace, url: db_refs[namespace]});
+
             }
 
           }
