7f83562e21ec4fb0815da3ebc49925bece64d4f6
Unknown <petar.v.todorov@gmail.com>
2017-10-03 14:51:50 -0400

    Fixes and lints to phosphoContextSN
    - one major change is the addition of `==` instead of a `=`
      in line 152, where previously `dragged` was being set to true
      where it should have been compared
    - it is alarming that this change does not break the code or
      lead to a major change in behavior...

--- static/js/phosphoContextSN.js
+++ static/js/phosphoContextSN.js
@@ -1,45 +1,47 @@
 function phosphoContextSN(cy, dataset, condition){
-    var removed = undefined;
-    for (a of ['mousedown', 'mouseup', 'touchstart', 'touchend', 'layoutstop']){
-     scapes['cy_1'].off(a)
+    var removed;
+    for (var a of ['mousedown', 'mouseup', 'touchstart', 'touchend', 'layoutstop']){
+     scapes['cy_1'].off(a);
     }
     var linear_scale = d3.scaleLinear()
-    .domain([-1, 0, 1])
-    .range(["blue", "white", "red"]);
-    cy.elements().forEach(function(e){e.addClass('original')})
+      .domain([-1, 0, 1])
+      .range(["blue", "white", "red"]);
+    cy.elements().forEach(function(e){
+      e.addClass('original');
+    });
     var nodes = [];
     var cmp_nodes = {};
     // var site_edges = [];
     var abundances = {};
-    var cond = dataset[condition]
-    var genes = Object.keys(cond)
+    var cond = dataset[condition];
+    var genes = Object.keys(cond);
     var radius;
     var small_radius;
-    for (g of genes){
-      var members = cond[g]['members']
-      var node = cy.$('node[name = ' +'"' + g + '"' + ']')
+    for (var g of genes){
+      var members = cond[g]['members'];
+      var node = cy.$('node[name = ' +'"' + g + '"' + ']');
       if (node.length > 0){
-         var abundance = cond[g]['node']
-         var abundance_values = abundance['values']
+         var abundance = cond[g]['node'];
+         var abundance_values = abundance['values'];
          if (abundance_values.length > 0){
-             var abundance_value = abundance_values[0]
-             abundances[node.data('id')] = linear_scale(abundance_value)
+             var abundance_value = abundance_values[0];
+             abundances[node.data('id')] = linear_scale(abundance_value);
          }
-         cmp_nodes[node.data('id')] = [node.data('id')]
-         var sites = members['sites']
-         radius = parseInt(node.style('width').substring(0, node.style('width').length-2))/2
-         var posx = node.position('x')
-         var posy = node.position('y')
-         var split = 12
-         small_radius = ((2*Math.PI*radius)/split)/2
+         cmp_nodes[node.data('id')] = [node.data('id')];
+         var sites = members['sites'];
+         radius = parseInt(node.style('width').substring(0, node.style('width').length-2))/2;
+         var posx = node.position('x');
+         var posy = node.position('y');
+         var split = 12;
+         small_radius = ((2*Math.PI*radius)/split)/2;
          // start small nodes at 4:30 position
-         var rad_coord = (2*Math.PI)*(1/8)
+         var rad_coord = (2*Math.PI)*(1/8);
          if (sites.length > 0){
-             var values = members['values']
+             var values = members['values'];
              for (var site in sites) {
-                var posx_a = posx + radius*Math.cos(rad_coord)
-                var posy_a = posy + radius*Math.sin(rad_coord)
-                rad_coord += (2*Math.PI)/10
+                var posx_a = posx + radius*Math.cos(rad_coord);
+                var posy_a = posy + radius*Math.sin(rad_coord);
+                rad_coord += (2*Math.PI)/10;
                 var node_site = {group: "nodes", data: {id: node.data('id')+'_'+site,
                                                        name: sites[site],
                                                        uuid_list: node.data('uuid_list'),
@@ -53,64 +55,59 @@ function phosphoContextSN(cy, dataset, condition){
                                                         'background-color':linear_scale(values[site]),
                                                         'z-index': 10,
                                                         'text-halign': 'right',
-                                                        'text-valign': 'center',
-                                                        'z-index': 10},
+                                                        'text-valign': 'center'},
                                                 position: {'x': posx_a,
                                                            'y': posy_a},
                                                 grabbable: false,
-                                                selectable: false}
-                nodes.push(node_site)
-                cmp_nodes[node.data('id')].push(node_site['data']['id'])
+                                                selectable: false
+                                };
+                nodes.push(node_site);
+                cmp_nodes[node.data('id')].push(node_site['data']['id']);
              }
          }
-
-         cy.add(nodes)
-
-         for (i in abundances){
-             var node = cy.$('node[id = ' +'"' + i + '"' + ']')
-             node.style('background-color', abundances[i])
+         cy.add(nodes);
+         for (var i in abundances){
+             var node = cy.$('node[id = ' +'"' + i + '"' + ']');
+             node.style('background-color', abundances[i]);
          }
       }
-
     }
 
-    for (n_id in cmp_nodes){
-        var n_ids = cmp_nodes[n_id].slice(1,cmp_nodes[n_id].length)
+    for (var n_id in cmp_nodes){
+        var n_ids = cmp_nodes[n_id].slice(1,cmp_nodes[n_id].length);
         var sub_selectors = [];
-        for (n2 of n_ids){
-            var selector_string_sub = 'node[id = ' +'"' + n2 + '"' + ']'
-            sub_selectors.push(selector_string_sub)
+        for (var n2 of n_ids){
+            var selector_string_sub = 'node[id = ' +'"' + n2 + '"' + ']';
+            sub_selectors.push(selector_string_sub);
         }
-        var selector_string = sub_selectors.join(',')
-        var sub_cy = cy.$(selector_string)
-        var node_tag = "sub"+n_id
+        var selector_string = sub_selectors.join(',');
+        var sub_cy = cy.$(selector_string);
+        var node_tag = "sub"+n_id;
         sub_cy.nodes().forEach(function(n){
-            n.addClass(node_tag)
-        })
+            n.addClass(node_tag);
+        });
     }
 
     cy.$().not('.original').nodes().forEach(function(n){
-        n.addClass('small_' + n.data('pos_diff')['main_id'])
-        n.addClass('small')
-    })
+        n.addClass('small_' + n.data('pos_diff')['main_id']);
+        n.addClass('small');
+    });
 
 
     function bindPosition(){
         cy.$('.original').on( 'position', function(evt){
             var node = evt.cyTarget;
-            var node_tag = ".small_"+node.data('id')
-            var posx = node.position('x')
-            var posy = node.position('y')
-            var associates = cy.$(node_tag)
+            var node_tag = ".small_"+node.data('id');
+            var posx = node.position('x');
+            var posy = node.position('y');
+            var associates = cy.$(node_tag);
             associates.nodes().forEach(function(assc){
                 assc.position({'x': (posx - assc.data('pos_diff')['dx']),
-                               'y': (posy - assc.data('pos_diff')['dy'])})
-            })
-        })
+                               'y': (posy - assc.data('pos_diff')['dy'])});
+            });
+        });
     }
-    bindPosition()
-
-
+    bindPosition();
 
     var params = {
       name: 'cola',
@@ -129,10 +126,10 @@ function phosphoContextSN(cy, dataset, condition){
     var pull_duration = 300;
     function pullNodes () {
         cy.$('.original').nodes().forEach(function(node){
-            var node_tag = ".small_"+node.data('id')
-            var posx = node.position('x')
-            var posy = node.position('y')
-            var associates = cy.$(node_tag)
+            var node_tag = ".small_"+node.data('id');
+            var posx = node.position('x');
+            var posy = node.position('y');
+            var associates = cy.$(node_tag);
             associates.nodes().forEach(function(assc){
                 assc.animate({
                     style: {'width': 1, 'height': 1,
@@ -140,63 +137,62 @@ function phosphoContextSN(cy, dataset, condition){
                     duration: pull_duration,
                     complete: function(){
                         if (removed === undefined){
-                                removed = cy.remove(assc)
-                                console.log('removed start!')
+                                removed = cy.remove(assc);
+                                console.log('removed start!');
                             }
                         else {
-                            removed = removed.add(cy.remove(assc))
-                            console.log('removed another!')
+                            removed = removed.add(cy.remove(assc));
+                            console.log('removed another!');
                         }
                         clearTimeout(animated_timer);
                         animated_timer = setTimeout(function() {
                             if (cy.$(':grabbed').length > 0){
                                 cy.promiseOn('free').then(function(){
                                     //console.log(event)
-                                    if (dragged = true){
-                                            console.log('freeeeeeeeedom!')
+                                    if (dragged == true){
+                                            console.log('freeeeeeeeedom!');
                                             clearTimeout(animated_timer);
                                             animated_timer = setTimeout(function() {
                                                 layout.run();
                                                 cy.promiseOn('layoutstop').then(function(){
                                                     if (removed !== undefined){
-                                                        cy.add(removed)
+                                                        cy.add(removed);
                                                         removed = undefined;
-                                                        pushNodes()
+                                                        pushNodes();
                                                     }
-                                                })
+                                                });
                                             }, 50);
                                     }
-                                })
+                                });
                             }
                             else {
-                                console.log('free already!')
-                                dragged = false
-                                layout.run()
+                                console.log('free already!');
+                                dragged = false;
+                                layout.run();
                                 cy.promiseOn('layoutstop').then(function(){
                                     if (removed !== undefined){
-                                        cy.add(removed)
+                                        cy.add(removed);
                                         removed = undefined;
-                                        pushNodes()
+                                        pushNodes();
                                     }
-                                })
+                                });
                             }
 
                         }, 50);
                     },
-
                     queue: false
-                })
-            })
-        })
+                });
+            });
+        });
     }
 
 
     function pushNodes () {
         cy.$('.original').nodes().forEach(function(node){
-            var node_tag = ".small_"+node.data('id')
-            var posx = node.position('x')
-            var posy = node.position('y')
-            var associates = cy.$(node_tag)
+            var node_tag = ".small_"+node.data('id');
+            var posx = node.position('x');
+            var posy = node.position('y');
+            var associates = cy.$(node_tag);
             associates.nodes().forEach(function(assc){
                 assc.animate({
                     style: {'width': small_radius*2, 'height':small_radius*2,
@@ -205,26 +201,22 @@ function phosphoContextSN(cy, dataset, condition){
                                              'y': (posy - assc.data('pos_diff')['dy'])}),
                     duration: pull_duration/3,
                     queue: true,
-                complete: function(){
-                    console.log('pushed nodes!')
-                    dragged = false
-                }
-                })
-            })
-        })
+                    complete: function(){
+                        console.log('pushed nodes!');
+                        dragged = false;
+                    }
+                });
+            });
+        });
     }
-
 
     var dragged = false;
     var animated;
     cy.$('.original').on(('drag'), function(){
         if (dragged === false){
-            console.log('drag!')
+            console.log('drag!');
             dragged = true;
-            pullNodes()
+            pullNodes();
         }
-    })
-
-
-
+    });
 }
